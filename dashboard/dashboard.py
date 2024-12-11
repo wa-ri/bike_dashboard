9ae31e72ddc299cc6ea08c2fd9de2fd86699217f
affrian <42446844+affrian@users.noreply.github.com>
2024-12-11 12:45:37 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 # Load the data
